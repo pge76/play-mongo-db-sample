8cdbba0495623b5cf421c2b07b14784f31448172
Patrick Gebhardt <patrick.gebhardt@gmail.com>
2020-11-22 02:24:02 +0100

    styling added

--- app/controllers/CarController.java
+++ app/controllers/CarController.java
@@ -35,12 +35,14 @@ public class CarController extends Controller {
     }
 
     public Result list(Http.Request request) {
-        cars = carRepository.findAll();
+        initlist();
         return ok(views.html.car.render(asScala(cars), form, request, messagesApi.preferred(request)));
     }
 
     public Result add(Http.Request request) {
         final Form<CarData> boundForm = form.bindFromRequest(request);
+
+        initlist();
 
         if(boundForm.hasErrors()) {
             logger.error("errors = {}", boundForm.errors());
@@ -53,7 +55,18 @@ public class CarController extends Controller {
 
             cars.add(car);
             return redirect(routes.CarController.list())
-                    .flashing("info", "Car added!");
+                    .flashing("Info", "Auto wurde hinzugefügt!");
+        }
+    }
+
+    private void initlist() {
+        try {
+            cars = carRepository.findAll();
+        } catch (Exception e) {
+            throw new RuntimeException("Cars could not be fetched from Database due to error", e);
+        }
+        if(cars == null) {
+            throw new RuntimeException("Cars could not be fetched from Database, result was null");
         }
     }
 }
